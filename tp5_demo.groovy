c053e1e3d9bc7d0ac8a73bca909b69a5134bba42
Pbuges <philippe.buges@ext.adeo.com>
2018-10-10 15:36:30 +0200

    ajout des scripts pour tp 4 jenkins

---
+++ tp5_demo.groovy
@@ -0,0 +1,13 @@
+node {
+    stage ('one'){
+        echo "kikou"
+        echo "joli"
+    }
+    stage ('two'){
+        echo "kikou"
+        echo "joli"
+    }
+     stage ('tree'){
+        echo "hostname"
+    }
+}
